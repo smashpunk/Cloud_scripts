b067415e28722538a6c774baf9386a0b47c7956e
smashpunk <smashpunk@gmail.com>
2018-01-26 09:42:19 +0800

    urgent update

---
+++ K8s/accessAPI.py
@@ -0,0 +1,10 @@
+from kubernetes import client, config
+config.list_kube_config_contexts('/root/.kube/config')
+# Configs can be set in Configuration class directly or using helper utility
+config.load_kube_config()
+
+v1 = client.CoreV1Api()
+print("Listing pods with their IPs:")
+ret = v1.list_pod_for_all_namespaces(watch=False)
+for i in ret.items:
+    print("%s\t%s\t%s" % (i.status.pod_ip, i.metadata.namespace, i.metadata.name))
